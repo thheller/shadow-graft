a63a4364acb2b3b60bdd24694ea38d1c0f7e349b
Thomas Heller <th.heller@gmail.com>
2022-06-30 21:00:32 +0200

    additional example

--- src/dev/demo/app.cljs
+++ src/dev/demo/app.cljs
@@ -1,7 +1,22 @@
 (ns demo.app
   (:require
     [shadow.graft.modules :as gm]
+    [shadow.graft :as graft]
     [cljs.reader :as reader]))
+
+(defmethod graft/scion "log" [_ _]
+  (graft/reloadable
+    (js/console.warn "html told me to log")))
+
+(defmethod graft/scion "toggle-section" [opts container]
+  ;; there are a variety of ways to get specific DOM elements
+  ;; I prefer data attribute over just class names or ids
+  (let [opener (.querySelector container "[data-opener]")
+        body (.querySelector container "[data-body]")]
+
+    (.addEventListener opener "click"
+      (fn [e]
+        (-> body .-classList (.toggle "hidden"))))))
 
 (defn init []
   (gm/init reader/read-string))
