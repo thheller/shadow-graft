59ea39b7b448f4825f38e067b7b33a6eef0221bf
Thomas Heller <th.heller@gmail.com>
2023-01-30 16:34:06 +0100

    bump 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.thheller/shadow-graft "0.7.0"
+(defproject com.thheller/shadow-graft "0.8.0"
   :description "Bridging function calls from CLJ to CLJS via HTML"
   :url "https://github.com/thheller/shadow-graft"
 
